7c852b3e6445557a91c8e42c106040c386dce9f5
AchuthaVVyas <80733877+AchuthaVVyas@users.noreply.github.com>
2021-04-30 12:12:44 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,8 +1,22 @@
+/**
+ * @file activity1.c
+ * @author Achutha Vyas
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "activity1.h"
 #include "activity2.h"
 #include "activity3.h"
 #include "activity4.h"
 
+/**
+ * @brief intialising input and output pins
+ * 
+ */
 void init_pins()
 {
     DDRB|=(1<<PB0);
@@ -12,12 +26,21 @@ void init_pins()
     PORTD|=(1<<PD3);
 }
 
+/**
+ * @brief LED ON
+ * 
+ */
 void led_on ()
 {
+
     PORTB|=(1<<PB0);
     _delay_ms(2000);
 }
 
+/**
+ * @brief LED OFF
+ * 
+ */
 void led_off ()
 {
      PORTB&=~(1<<PB0);
